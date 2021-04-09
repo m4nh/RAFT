0af5e0ec319d84f2d1aec35f49285230c0079071
m4nh <degregorio.daniele@gmail.com>
2021-04-09 18:58:57 +0200

    Refactoring

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
+import torch.nn.functional as F
+import kornia.geometry as geometry
 import sys
-sys.path.append('core')
-
 import argparse
 import os
 import cv2
@@ -8,14 +8,13 @@ import glob
 import numpy as np
 import torch
 from PIL import Image
-
-from raft import RAFT
-from utils import flow_viz
-from utils.utils import InputPadder
-
+from core.raft import RAFT
+from core.utils import flow_viz
+from core.utils.utils import InputPadder
 
 
 DEVICE = 'cuda'
+
 
 def load_image(imfile):
     img = np.array(Image.open(imfile)).astype(np.uint8)
@@ -23,10 +22,17 @@ def load_image(imfile):
     return img[None].to(DEVICE)
 
 
+def view(img, name):
+    img = img[0].permute(1, 2, 0).cpu().numpy().astype(np.uint8)
+
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.imshow(name, img)
+
+
 def viz(img, flo):
-    img = img[0].permute(1,2,0).cpu().numpy()
-    flo = flo[0].permute(1,2,0).cpu().numpy()
-    
+    img = img[0].permute(1, 2, 0).cpu().numpy()
+    flo = flo[0].permute(1, 2, 0).cpu().numpy()
+
     # map flow to rgb image
     flo = flow_viz.flow_to_image(flo)
     img_flo = np.concatenate([img, flo], axis=0)
@@ -35,7 +41,8 @@ def viz(img, flo):
     # plt.imshow(img_flo / 255.0)
     # plt.show()
 
-    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.imshow('image', img_flo[:, :, [2, 1, 0]]/255.0)
     cv2.waitKey()
 
 
@@ -49,8 +56,8 @@ def demo(args):
 
     with torch.no_grad():
         images = glob.glob(os.path.join(args.path, '*.png')) + \
-                 glob.glob(os.path.join(args.path, '*.jpg'))
-        
+            glob.glob(os.path.join(args.path, '*.jpg'))
+
         images = sorted(images)
         for imfile1, imfile2 in zip(images[:-1], images[1:]):
             image1 = load_image(imfile1)
@@ -60,6 +67,18 @@ def demo(args):
             image1, image2 = padder.pad(image1, image2)
 
             flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
+
+            ############
+            height, width = image1.shape[-2:]
+            grid = geometry.create_meshgrid(height, width, normalized_coordinates=False).to(image1.device)
+            print("SPODSAOPDA", flow_up.shape, grid.shape, grid.min(), grid.max())
+            grid = flow_up.permute(0, 2, 3, 1) + grid
+            flow_up_norm = geometry.normalize_pixel_coordinates(grid, height, width)  # BxHxWx2
+
+            image1_warped = F.grid_sample(image2, flow_up_norm, align_corners=True)
+
+            view(image1_warped, 'img2_warped')
+            view(image1, 'img2')
             viz(image1, flow_up)
 
 
